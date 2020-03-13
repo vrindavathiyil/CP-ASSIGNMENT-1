6a6ce40d50692025c5cc991d696fa5e1223ec59a
VRINDA VATHIYIL <62105596+vrindavathiyil@users.noreply.github.com>
2020-03-13 15:06:49 +0530

    Create to display Floyd's triangle.c

---
+++ to display Floyd's triangle.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+void main()
+
+	{		int r,n=1;
+
+	
+
+		printf("\n Enter the number of rows: ");
+
+		scanf("%d",&r);
+
+	
+
+		printf("\n Floyd's triangle containing %d rows is...\n",r);
+
+	
+
+		for(int i=0;i<r;i++)
+
+			{
+
+	  			for(int j=0;j<=i;j++)
+
+	  				{	
+
+		  				printf("%d ",n);
+
+		  				n++;
+
+	  				}
+
+	  			
+
+				printf("\n");
+
+		         }
+
+	}
