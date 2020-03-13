9d810aaf42fde6e4985138b2849ab9f4506549ab
VRINDA VATHIYIL <62105596+vrindavathiyil@users.noreply.github.com>
2020-03-13 17:16:18 +0530

    Create value of sin(x) and cos(x).c

---
+++ value of sin(x) and cos(x).c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+void main()
+    {
+        
+       double sin value,cos value,x;
+    
+
+       printf("\nEnter the value");
+       scanf("%f",&x);
+       sin value=sin(x);
+       cos value=cos(x);
+       printf("\n The sin value of %f=%f",x,sin value);
+       printf("\n The cos value of %f=%f",x,cos value);
+       
+       
+     }
