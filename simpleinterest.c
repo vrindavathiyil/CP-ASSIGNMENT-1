32b2894a5e2bdfbb9f32f05bc9442c3e3fe6a767
VRINDA VATHIYIL <62105596+vrindavathiyil@users.noreply.github.com>
2020-03-13 15:18:25 +0530

    Update simpleinterest.c

--- simpleinterest.c
+++ simpleinterest.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
-void main()
+int main()
          { 
-           float principle,time,rate,SI;
-           printf("Enter the principle amount");
-           scanf("%f",&principle);
-           printf("Enter time");
-           scanf("%f",&time);
-           printf("Enter rate");
-           scanf("%f",&rate);
-           SI=(principle*time*rate)/100;
-           printf("Simple interest=%f",SI);
+          
+            float principle,time,rate,SI;
+            
+            /* Input principle, time and rate */
+            printf("Enter the principle amount: ");
+            scanf("%f",&principle);
+         
+            printf("Enter time");
+            scanf("%f",&time);
+         
+            printf("Enter rate");
+            scanf("%f",&rate);
+         
+            /* Calculate simple interest */ 
+            SI=(principle*time*rate) / 100;
+         
+            /* Print the resultant value of SI */
+            printf("Simple interest=%f", SI);
+         
+            return 0;
+         
           }
            
            
